48e174667551f1bbef5545d943301736feea3cdf
TRIGUNTEJ <triguncherry@gmail.com>
2024-03-06 09:31:00 +0530

    Auto Login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriver driver;
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+        driver.findElement(By.id("ap_email")).sendKeys("balubalabrahmaiah@gmail.com");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("ap_password")).sendKeys("Ap26l3159@");
+        driver.findElement(By.id("signInSubmit")).click();
     }
 }
