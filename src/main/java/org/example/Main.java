a5220c12f4fbc5d01dfe56aabe66f17bdd959a7a
KLU_2100031077 <112616971+TRIGUNTEJ@users.noreply.github.com>
2024-03-06 09:38:29 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,9 @@ public class Main {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
-        driver.findElement(By.id("ap_email")).sendKeys("balubalabrahmaiah@gmail.com");
+        driver.findElement(By.id("ap_email")).sendKeys("*******************************");
         driver.findElement(By.id("continue")).click();
-        driver.findElement(By.id("ap_password")).sendKeys("Ap26l3159@");
+        driver.findElement(By.id("ap_password")).sendKeys("**************");
         driver.findElement(By.id("signInSubmit")).click();
     }
 }
